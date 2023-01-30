a72a52bfc5d006afc88cc0422a529c48b071a2df
Leandro de Oliveira <leandroparipe@hotmail.com>
2023-01-30 17:35:55 -0300

    Finalizado o teste de tecnologia

--- Payment-api.WebAPI/Program.cs
+++ Payment-api.WebAPI/Program.cs
@@ -18,7 +18,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.RoutePrefix = string.Empty;
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+});
 
 app.UseHttpsRedirection();
 
