0375e6c8d1c8c9e9c5d2b834e41dd5615cfe1cb2
leandroliveirac <leandroparipe@hotmail.com>
2023-06-24 16:19:17 -0300

    configuracao ambiente de execução

--- src/Payment-api.WebAPI/Program.cs
+++ src/Payment-api.WebAPI/Program.cs
@@ -3,6 +3,12 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json",true,true)
+        .AddJsonFile($"appsettings{builder.Environment.EnvironmentName}.json",true,true)
+        .AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddInfrastructure(builder.Configuration);
 
